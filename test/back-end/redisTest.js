d8c09969658c4b9296848261d660cae87469d9fb
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-14 15:18:10 +0100

    added asyn call manager to redis.js and built initial db tests

--- test/back-end/redisTest.js
+++ test/back-end/redisTest.js
@@ -5,14 +5,49 @@ var db = require('../../server/redis.js');
 
 db.startDB();
 
-test('woah', function(t){
-  db.postQuestion({test: 'woah!'}, function(data) {
-    db.getQuestion(1, function(data) {
-      t.equal(data.test, 'woah!')
+var caller = db.createCaller();
+
+
+test('basic server test', function(t) {
+  caller.add(function(next) {
+    db.postQuestion({test: 'woah!'}, function(data) {
+      t.deepEqual(data, {test: 'woah!'}, 'post question returns what was posted');
+      next();
+    });
+  }).add(function(next) {
+    db.postQuestion({
+        id: '12',
+        someText: 'ipso bla bla'
+    }, function(data) {
+      next();
+    });
+  }).add(function(next) {
+    db.postQuestion({test: 'woah!'}, function(data) {
+      console.log('call one: ', data);
+      next();
+    });
+  }).add(function(next) {
+     db.getQuestion(2, function(data) {
+       t.deepEqual(data, {
+           id: '12',
+           someText: 'ipso bla bla'
+       }, 'getQuestion gets what was posted');
+       next();
+    });
+  }).add(function(next) {
+    db.deleteLastQuestion(function() {
       db.deleteLastQuestion(function() {
-        t.end()
-        db.stopDB();
+        db.deleteLastQuestion(function() {
+          next();
+        });
       });
     });
+  }).add(function(next) {
+    db.questionCount(function(count) {
+      t.equal(count, '0', 'no questions in db after tests');
+      db.stopDB();
+      t.end();
+    });
   });
+  caller();
 });
