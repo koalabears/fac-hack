0f1dc82b86798ac98a5e770cf40771dfee00c8b3
Naaz <marziyeh.naaz.ahmed@gmail.com>
2015-10-14 20:30:00 +0100

    linking posts to redis

--- server/handler.js
+++ server/handler.js
@@ -1,14 +1,20 @@
 var fs = require('fs');
 var index = fs.readFileSync(__dirname + '/../public/html/index.html');
+var indexJS = fs.readFileSync(__dirname + '/../public/js/main.js');
 
-var handler = function(req, res){
+var handler = function(req, res) {
   var url = req.url;
   console.log(url);
-  if(url === '/'){
+  if (url === '/') {
     res.writeHead(200, {
       'Content-Type': 'text/html'
     });
     res.end(index);
+  } else if (url === '/main.js') {
+    res.writeHead(200, {
+      'Content-Type': 'text/js'
+    });
+    res.end(indexJS);
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html'
@@ -16,6 +22,5 @@ var handler = function(req, res){
     res.end();
   }
 };
-
 
 module.exports = handler;
