9a9f019fb9e43a699bca2d139c5e302c682422fc
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-10-16 12:20:49 +0100

    clearing up comments

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@ var qAll=document.getElementById('questionsAll');
 form.addEventListener('submit', emitMsg);
 
 var token = window.location.search.split('=')[1];
-// window.history.pushState(“string”, auth, “/#”);
 
 function emitMsg(e){
   e.preventDefault();
@@ -12,8 +11,6 @@ function emitMsg(e){
   var newQ = document.getElementById('newQ');
   socket.emit('question in', input.value.replace(/<.*>/g, ''));
   input.value = '';
-  // input.value = input.value;
-  // console.log(input.value);
 }
 socket.on('question out', function(msg){
   var username="marie";
@@ -36,12 +33,10 @@ socket.on('question out', function(msg){
 
 function createPageHtml(data){
   var question ="<div>";
-  // question +=
   for (i = data.length-1; i >= 0; i--) {
-  // for(i=0;i<=data.length;i++){"
     question += "<div class=q id=q" + i + "><a href='/question" + data[i].id +"'>" + data[i].question + "</a><br></div>";
     question += "<div class=details" + ">"+  "Username: " + data[i].name + "<br>" + "Date: " + data[i].date + "<br><br></div>";
-    // question += "<div class=details" + ">" "<br></div>";
+
    }
    question += "</div>";
     qAll.innerHTML = question;
